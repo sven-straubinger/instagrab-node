44e30af67e75cd337213a47bf9c6741adbf527af
Sven Straubinger <straubinger.es@googlemail.com>
2016-07-11 00:05:47 +0200

    Display markers on component updates

--- build/bundle.js
+++ build/bundle.js
@@ -112,20 +112,18 @@ var Map = React.createClass({
 
   componentDidMount: function () {
     //  Initialize map
-    var map = new google.maps.Map(document.getElementById('map'), {
+    this.map = new google.maps.Map(document.getElementById('map'), {
       center: { lat: 52.522307, lng: 13.399151 },
       zoom: 10
     });
-
-    // Add markers
-    for (var index in this.props.markers) {
-      var marker = this.props.markers[index];
-      this.addMarker(marker.lat, marker.lng, map);
-    }
   },
 
   componentDidUpdate: function () {
-    console.log("Map component did update.");
+    // Update markers
+    for (var index in this.props.markers) {
+      var marker = this.props.markers[index];
+      this.addMarker(marker.lat, marker.lng, this.map);
+    }
   },
 
   addMarker: function (lat, lng, map) {
