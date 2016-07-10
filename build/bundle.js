b0c2a2f73ce784f39735f9de2bbd19ddff55acac
Sven Straubinger <straubinger.es@googlemail.com>
2016-07-10 22:44:48 +0200

    Update request handling

--- build/bundle.js
+++ build/bundle.js
@@ -10,17 +10,49 @@ var App = React.createClass({
 
 
   statics: {
-    baseUrl: 'https://api.github.com/users/octocat/gists'
+
+    instagram: {
+
+      baseUrl: 'https://api.instagram.com/v1/media/search',
+      accessToken: '' // <-- Please enter a valid access-token here
+
+      /*
+       * NOTE:
+       * Even though our access tokens do not specify an expiration time,
+       * the app should handle the case that either the user revokes access,
+       * or Instagram expires the token after some period of time.
+       *
+       * If the token is no longer valid, API responses will contain an “error_type=OAuthAccessTokenError”.
+       * In this case we will need to re-authenticate the user to obtain a new valid token.
+       * In other words: we should not assume our access_token is valid forever.
+       *
+       * For the sake of simplicity we assume the token does not expire.
+       *
+       * See: https://www.instagram.com/developer/authentication/
+       */
+
+    }
   },
 
   getInitialState: function () {
     return {
-      markers: [{ id: 1, title: "Title-1", lat: 52.522307, lng: 13.398251 }, { id: 2, title: "Title-1", lat: 52.523407, lng: 13.399151 }]
+      markers: [
+        // {id: 1, title: "Title-1", lat: 52.522307, lng: 13.398251},
+        // {id: 2, title: "Title-1", lat: 52.523407, lng: 13.399151},
+      ]
     };
   },
 
   componentDidMount: function () {
-    this.serverRequest = jQuery.get(App.baseUrl, function (result) {
+    var parameters = {
+      lat: 52.522307,
+      lng: 13.398251,
+      access_token: App.instagram.accessToken
+    };
+
+    var url = App.instagram.baseUrl + "?" + jQuery.param(parameters);
+
+    this.serverRequest = jQuery.get(url, function (result) {
       var lastGist = result[0];
       this.setState({
         // markers: // Replace with response
