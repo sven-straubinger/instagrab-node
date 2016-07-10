d20193923122fcc4e9e15d0c06f8a2678670ed2b
Sven Straubinger <straubinger.es@googlemail.com>
2016-07-10 15:46:51 +0200

    Add helper function

--- build/bundle.js
+++ build/bundle.js
@@ -35,10 +35,19 @@ var Map = React.createClass({
       center: { lat: -34.397, lng: 150.644 },
       zoom: 8
     });
+    this.addMarker(-34.397, 150.644, map);
   },
 
   componentDidUpdate: function () {
     console.log("Map component did update.");
+  },
+
+  addMarker: function (lat, lng, map) {
+    var latLng = new google.maps.LatLng(lat, lng);
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
   },
 
   render: function () {
--- components/map.js
+++ components/map.js
@@ -7,10 +7,21 @@ var Map = React.createClass({
       center: {lat: -34.397, lng: 150.644},
       zoom: 8
     });
+    this.addMarker(-34.397, 150.644, map);
   },
 
   componentDidUpdate: function() {
     console.log("Map component did update.")
+  },
+
+  addMarker: function(lat, lng, map) {
+    var latLng = new google.maps.LatLng(lat,lng);
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map,
+      // title: 'Title',
+      // icon: { ... some icon or style ... }
+    });
   },
 
   render: function() {
