defc71e68a7340a54a555edbb5df191bc9633a0a
Sven Straubinger <straubinger.es@googlemail.com>
2016-07-10 16:03:25 +0200

    Add mocked data source

--- components/app.js
+++ components/app.js
@@ -2,13 +2,20 @@ var React = require('react');
 var Map = require('./map');
 var Navigation = require('./navigation');
 
+// Data Source
+var markers = [
+  {id: 1, title: "Title-1", lat: -34.387, lng: 150.634},
+  {id: 2, title: "Title-2", lat: -34.397, lng: 150.654}
+];
+
+// App Component
 var App = React.createClass({
   render: function() {
     return (
       <div>
         <h1>Instagrab</h1>
         <Navigation />
-        <Map />
+        <Map markers={markers} />
       </div>
     );
   }
