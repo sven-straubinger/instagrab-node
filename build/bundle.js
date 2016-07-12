8e9f7db78e58643286ddb67b515948cac3913e28
Sven Straubinger <straubinger.es@googlemail.com>
2016-07-12 02:57:36 +0200

    Format code

--- build/bundle.js
+++ build/bundle.js
@@ -13,17 +13,7 @@ var App = React.createClass({
 
 
   statics: {
-
     instagram: {
-
-      accessToken: '50913539.1a5def0.6b4340c5e992471688d9f1ec5cad48c2', // Please enter a valid access-token here
-      searchDistance: 3000, // Instagram default is 1km (distance=1000), max distance is 5km
-      searchEndpoint: function () {
-        return 'https://api.instagram.com/v1/media/search';
-      },
-      likeEndpoint: function (id) {
-        return 'https://api.instagram.com/v1/media/' + id + '/likes';
-      }
 
       /*
        * NOTE:
@@ -40,6 +30,14 @@ var App = React.createClass({
        * See: https://www.instagram.com/developer/authentication/
        */
 
+      accessToken: '50913539.1a5def0.6b4340c5e992471688d9f1ec5cad48c2', // Please enter a valid access-token here
+      searchDistance: 3000, // Instagram default is 1km (distance=1000), max distance is 5km
+      searchEndpoint: function () {
+        return 'https://api.instagram.com/v1/media/search';
+      },
+      likeEndpoint: function (id) {
+        return 'https://api.instagram.com/v1/media/' + id + '/likes';
+      }
     }
   },
 
